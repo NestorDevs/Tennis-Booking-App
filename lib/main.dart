01747b169f2940d974ee3759a80f0905e3f456aa
NestorDev <neivertjosias@gmail.com>
2025-07-28 15:58:26 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tennis_booking_app/data/repositories/weather_repository_impl.dart';
-import 'data/datasources/weather_remote_data_source.dart';
-import 'domain/repositories/weather_repository.dart';
-import 'domain/usecases/get_rain_probability.dart';
-import 'presentation/bloc/booking_list/booking_list_event.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/theme/app_theme.dart';
-import 'presentation/bloc/theme/theme_cubit.dart';
-
 import 'data/datasources/booking_local_datasource.dart';
+import 'data/datasources/weather_remote_data_source.dart';
 import 'data/repositories/booking_repository_impl.dart';
+import 'data/repositories/weather_repository_impl.dart';
 import 'domain/repositories/booking_repository.dart';
+import 'domain/repositories/weather_repository.dart';
 import 'domain/usecases/add_booking.dart';
 import 'domain/usecases/delete_booking.dart';
 import 'domain/usecases/get_all_bookings.dart';
+import 'domain/usecases/get_rain_probability.dart';
 import 'presentation/bloc/add_booking/add_booking_bloc.dart';
 import 'presentation/bloc/booking_list/booking_list_bloc.dart';
+import 'presentation/bloc/booking_list/booking_list_event.dart';
+import 'presentation/bloc/theme/theme_cubit.dart';
 import 'presentation/screens/home_screen.dart';
 
 void main() async {
